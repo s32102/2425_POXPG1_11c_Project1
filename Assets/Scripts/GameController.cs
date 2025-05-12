ef6c6dde7208f0dfe7f9b644825bb2a4334153cd
s32102 <s32102@pjwstk.edu.pl>
2025-05-12 15:32:51 +0200

    praca nad gra na zajeciach

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,20 +7,78 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public string WORD = "after";
+    public int livesCount = 10;
     public TextMeshProUGUI wordtxt;
-    public TextMeshProUGUI OurInput
-    public InputField InputField
+    public TextMeshProUGUI messageText;
+    public TextMeshProUGUI livesCountText;
+    public TMP_InputField InputField;
 
     // Start is called before the first frame update
     void Start()
     {
         wordtxt.text = WORD;  
+        livesCountText.text =$"Zsotało {livesCount} prób";
     }
+    
+    public void OnClick()
+    {
+        Debug.Log($"Guzik został kliknięty");
+        livesCount -= 1;
+        livesCountText.text =$"Zsotało {livesCount} prób";
+
+        if(WORD == InputField.text)
+        {
+            messageText.text = $"Wpisano poprawne słowo";
+            return; //Koniec metody
+        }
+        if(WORD.Length != InputField.text.Length)
+        {
+            messageText.text = $"Liczba liter się nie zgadza";
+            return; //Koniec metody
+        }
+        int bullsCount = CountBulls();
+        int cowsCount = CountCows();
+        messageText.text = $"Bulls: {bullsCount} and Cows: {cowsCount}";
+
+    }
+
+    public int CountBulls()
+    {
+        int result = 0;
+
+        for (int i = 0; i < WORD.Length; i++)
+        {
+            if (WORD[i] == InputField.text[i])
+            {
+                result++;
+            }
+        }
+        return result;
+     
+    }
+     public int CountCows()
+    {
+       
+       int result = 0;
+
+        for (int i = 0; i < WORD.Length; i++)
+        {
+            if (WORD[i] != InputField.text[i] && WORD.Contains(InputField.text[i]))
+            {
+                result++;
+            }
+        }
+        return result;
+     
+    }
+
 
     public void OnInputChanged(string input)
     {
-        OurInputChanged.text = InputField;
+        //OurInput.text = InputField.text;
     }
+
+
     // Update is called once per frame
     void Update()
     {
